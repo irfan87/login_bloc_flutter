0586aa726effce9d39090125e503af8c3083d4bc
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-04 15:59:40 +0800

    ui for login screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -5,8 +5,42 @@ class LoginScreen extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: [Text('Log me in')],
+        children: [
+          emailInput(),
+          passwordInput(),
+          SizedBox(
+            height: 8.0,
+          ),
+          loginButton(),
+        ],
       ),
+    );
+  }
+
+  Widget emailInput() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@email.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordInput() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Passsword',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget loginButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('Login'),
     );
   }
 }
